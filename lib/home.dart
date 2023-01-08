f3a8100e7a2506c1b4d6e1a848ee92c2a91028e8
まさたか <shinonome419@icloud.com>
2023-01-08 11:49:48 +0900

    clean: 重複しているMyHomePageを削除

--- lib/home.dart
+++ lib/home.dart
@@ -2,20 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'encounter.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'エンカウント画面'),
-    );
-  }
-}
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
